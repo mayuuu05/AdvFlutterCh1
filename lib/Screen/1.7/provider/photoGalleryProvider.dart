3b50d58a17bc2750785c0532bec28068b05102e8
mayuri <mayup95106@gmail.com>
2024-07-18 12:32:09 +0530

    ...

--- lib/Screen/1.7/provider/photoGalleryProvider.dart
+++ lib/Screen/1.7/provider/photoGalleryProvider.dart
@@ -8,43 +8,26 @@ import 'package:local_auth/local_auth.dart';
 class Photogalleryprovider extends ChangeNotifier{
 
   final LocalAuthentication auth = LocalAuthentication();
-  bool _didAuthenticate = false;
+  bool Fingerprint = false;
 
   Future<void> localUserAuthentication(BuildContext context) async {
     List<BiometricType> availableBiometrics;
     try {
       availableBiometrics = await auth.getAvailableBiometrics();
-    } on PlatformException catch (e) {
-
-      return;
-    }
-
-    if (availableBiometrics.isEmpty) {
-
-      return;
-    }
-
+    } on PlatformException catch (e) {}
     try {
       final didAuthenticate = await auth.authenticate(
-        localizedReason: 'Please authenticate to access hide folder',
+        localizedReason: 'Verify with Fingerprint',
       );
-      _didAuthenticate = didAuthenticate;
+      Fingerprint = didAuthenticate;
       notifyListeners();
-    } on PlatformException catch (e) {
+    } on PlatformException catch (e) {}
 
-    }
-
-    if (_didAuthenticate) {
+    if (Fingerprint) {
       Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => const Hidephotogallery(),
       ));
-    } else {
     }
   }
 
-  int randomNumber() {
-    Random random = Random();
-    int a = random.nextInt(1100);
-    return a;
-  }
 }
